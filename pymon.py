564748ea0569b11589c183b2a4cd415fa0f7f793
Aroop 'Finlay' Roelofs <me@finlaydag33k.nl>
2016-02-24 17:47:47 +0100

    Update pymon.py
    - Fixed a bug with higher ports (25565 for example)
    - Added a default port (port 80)
    - Added support for multiple servers

--- pymon.py
+++ pymon.py
@@ -1,29 +1,21 @@
-## Script By FinlayDaG33k under the MIT License ##
 import os 
 import RPi.GPIO as gpio
 import time
 import socket
 
 ## set variables for the machine to ping and pin for the LED
-hostname = "kandicraft.finlaydag33k.nl"
-port = 25565
+hostname = ['kandicraft.finlaydag33k.nl:25565','kandicraft.finlaydag33k.nl:80','www.finlaydag33k.nl']
 led_pin = 37
 
-## prepare
+## prepare the GPIO
 led_status = gpio.LOW
 gpio.setmode(gpio.BOARD)
 gpio.setup(led_pin, gpio.OUT, gpio.PUD_OFF, led_status)
 
-## the main ping function
-def check_ping(host, port):
+## PING FUNCTION GALORE!!
+def check_ping(host,port):
     captive_dns_addr = ""
     host_addr = ""
-
-    try:
-        captive_dns_addr = socket.gethostbyname("BlahThisDomaynDontExist22.com")
-    except:
-        pass
-
     try:
         host_addr = socket.gethostbyname(host)
 
@@ -32,9 +24,9 @@ def check_ping(host, port):
 
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(1)
-        s.connect((host, port))
+        s.connect((host,port))
         s.close()
-    except:
+    except Exception as exc:
         return False
 
     return True
@@ -43,12 +35,24 @@ def check_ping(host, port):
 	
 ## Run the script itself infinitely
 while True:
-    pingstatus = check_ping(hostname,port)
-    if pingstatus == False:
-        print('Setting pin to HIGH')
+    host_up = ""
+    for host in hostname:
+        if ":" in host:
+            temphost = ""
+            temphost, tempport = host.split(":")
+            tempport = int(tempport)
+        else: 
+            temphost = host
+            tempport = 80
+        pingstatus = check_ping(temphost, tempport)
+        if pingstatus == False:
+            print('[' + time.strftime("%d-%m-%Y %H:%M:%S") + '] ' + temphost + ' on port ' + tempport + ' seems to be unreachable!')
+            host_up = "False"
+
+    if host_up == "False":
         led_status = gpio.HIGH
     else:
-        print('Setting pin to LOW')
         led_status = gpio.LOW
-	gpio.output(led_pin,led_status) # set the LED to it's proper position
-	time.sleep(10) # wait for 10 seconds to rerun the loop
+    gpio.output(led_pin,led_status) # set the LED to it's proper position
+    time.sleep(10) # wait for 10 seconds to rerun the loop
+
